e7b6020349d5848658fc6993f9aa5dabc0d2e9be
123@qwe <hashemkhajepour@yahoo.com>
2023-03-13 19:06:40 +0330

    add repository for generic ripository

---
+++ College.EFDataAccessLayer/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using College.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace College.EFDataAccessLayer.Repositories.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity
+    {
+        DbSet<TEntity> Entities { get; }
+        IQueryable<TEntity> Table { get; }  
+        IQueryable<TEntity> TableNoTracking { get; }
+
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        void Attach(TEntity entity);
+        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        void Detach(TEntity entity);
+        ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids);
+        Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken) where TProperty : class;
+        Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken) where TProperty : class;
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+    }
+}
---
+++ College.EFDataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,95 @@
+﻿using College.EFDataAccessLayer.Repositories.Contracts;
+using College.Entities;
+using College.Persistence.EF;
+using Common.Utilities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace College.EFDataAccessLayer.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        protected readonly EFCollegeContext DbContext;
+        public DbSet<TEntity> Entities { get; }
+        public virtual IQueryable<TEntity> Table => Entities;
+        public virtual IQueryable<TEntity> TableNoTracking => Entities.AsNoTracking();
+
+        public Repository(EFCollegeContext dbContext)
+        {
+            DbContext = dbContext;
+            Entities = DbContext.Set<TEntity>(); // City => Cities
+        }
+
+        #region Async Method
+        public virtual ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
+        {
+            return Entities.FindAsync(ids, cancellationToken);
+        }
+
+        public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Update(entity);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Remove(entity);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+        #endregion
+
+
+
+        #region Attach & Detach
+        public virtual void Detach(TEntity entity)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            var entry = DbContext.Entry(entity);
+            if (entry != null)
+                entry.State = EntityState.Detached;
+        }
+
+        public virtual void Attach(TEntity entity)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            if (DbContext.Entry(entity).State == EntityState.Detached)
+                Entities.Attach(entity);
+        }
+        #endregion
+
+        #region Explicit Loading
+        public virtual async Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken)
+            where TProperty : class
+        {
+            Attach(entity);
+
+            var collection = DbContext.Entry(entity).Collection(collectionProperty);
+            if (!collection.IsLoaded)
+                await collection.LoadAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual async Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken)
+            where TProperty : class
+        {
+            Attach(entity);
+            var reference = DbContext.Entry(entity).Reference(referenceProperty);
+            if (!reference.IsLoaded)
+                await reference.LoadAsync(cancellationToken).ConfigureAwait(false);
+        }
+        #endregion
+    }
+}
